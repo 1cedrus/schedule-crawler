0213bf096ccd44513f9794c994eef536bd37c250
1cedrus <aowiej23s@gmail.com>
2025-05-21 17:06:43 +0700

    Add return schedule acsii

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net"
@@ -12,6 +13,7 @@ import (
 )
 
 const layout = "02/01/2006"
+const layout2 = "2006-01-02T04:05:06"
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	t := r.PathValue("t")
@@ -25,10 +27,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	scheduleResp, err := qldt.FetchDSTKB(tokenResp.AccessToken, tokenResp.Name)
-    if err != nil {
-        fmt.Fprintf(w, "Error occured when fetching schedule data!\n%v\n", err)
-        return
-    }
+	if err != nil {
+		fmt.Fprintf(w, "Error occured when fetching schedule data!\n%v\n", err)
+		return
+	}
 
 	now := time.Now()
 	if len(t) > 0 {
@@ -46,13 +48,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		startTime, err := time.Parse(layout, value.NgayBatDau)
 		if err != nil {
 			fmt.Fprintf(w, "Error occured when parsing schedule data!\n%v\n", err)
-            continue 
+			continue
 		}
 
 		endTime, err := time.Parse(layout, value.NgayKetThuc)
 		if err != nil {
 			fmt.Fprintf(w, "Error occured when parsing schedule data!\n%v\n", err)
-            continue
+			continue
 		}
 
 		if now.After(startTime) && now.Before(endTime.Add(24*time.Hour-time.Nanosecond)) {
@@ -61,7 +63,131 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	fmt.Fprintf(w, "%v\n", curTuanTKB)
+	tempTKB := make([]qldt.ThoiKhoaBieu, 0)
+	for _, value := range curTuanTKB.DSThoiKhoaBieu {
+		if len(tempTKB) == 0 {
+			tempTKB = append(tempTKB, value)
+			continue
+		}
+
+		ngayHoc, err := time.Parse(layout2, value.NgayHoc)
+		if err != nil {
+			fmt.Fprintf(w, "Error occured when parsing schedule data!\n%v\n", err)
+			continue
+		}
+
+		currNgayHoc, err := time.Parse(layout2, tempTKB[0].NgayHoc)
+		if err != nil {
+			fmt.Fprintf(w, "Error occured when parsing schedule data!\n%v\n", err)
+			continue
+		}
+
+		if currNgayHoc.Equal(ngayHoc) {
+			tempTKB = append(tempTKB, value)
+			continue
+		}
+
+		scheduleBytes, err := generateSchedule(tempTKB, scheduleResp)
+        if err != nil {
+            fmt.Fprintf(w, "Error occured when generating schedule data!\n%v\n", err)
+            return
+        }
+
+		tempTKB = append([]qldt.ThoiKhoaBieu{}, value)
+		fmt.Fprintf(w, "%v", string(scheduleBytes))
+	}
+
+    scheduleBytes, err := generateSchedule(tempTKB, scheduleResp)
+    if err != nil {
+        fmt.Fprintf(w, "Error occured when generating schedule data!\n%v\n", err)
+        return
+    }
+
+	fmt.Fprintf(w, "%v", string(scheduleBytes))
+}
+
+func generateSchedule(tkb []qldt.ThoiKhoaBieu, scheduleResp *qldt.ScheduleResponse) ([]rune, error) {
+	ngayHoc, err := time.Parse(layout2, tkb[0].NgayHoc)
+	if err != nil {
+		return nil, fmt.Errorf("Error occured when parsing schedule data!\n%v\n", err)
+	}
+
+	scheduleBytes := []rune(RefTop)
+
+	var tmp bytes.Buffer
+	fmt.Fprintf(&tmp, "%v %.02d %v", (ngayHoc.Weekday() + 3).String()[:3], ngayHoc.Day(), ngayHoc.Month())
+
+	copy(scheduleBytes[(Colums*NgaySpot.Row)+NgaySpot.StartIndex:(Colums*NgaySpot.Row)+NgaySpot.StartIndex+NgaySpot.Length], []rune(tmp.String()))
+
+	for i := 0; i < len(tkb); i++ {
+		var ref []rune
+
+		tiet := tkb[i]
+
+		thoiGianBatDau := scheduleResp.Data.DSTietTrongNgay[tiet.TietBatDau-1].GioBatDau
+		thoiGianKetThuc := scheduleResp.Data.DSTietTrongNgay[tiet.TietBatDau+tiet.SoTiet-1].GioKetThuc
+
+		if i != len(tkb)-1 {
+			ref = []rune(RefMiddle)
+
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v - %v", thoiGianBatDau, thoiGianKetThuc)
+			copy(ref[(Colums*ThoiGianSpot.Row)+ThoiGianSpot.StartIndex:(Colums*ThoiGianSpot.Row)+ThoiGianSpot.StartIndex+ThoiGianSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.SoTiet)
+			copy(ref[(Colums*TietSpot.Row)+TietSpot.StartIndex:(Colums*TietSpot.Row)+TietSpot.StartIndex+TietSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.TenMon)
+			copy(ref[(Colums*MonSpot.Row)+MonSpot.StartIndex:(Colums*MonSpot.Row)+MonSpot.StartIndex+MonSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.MaCoSo)
+			copy(ref[(Colums*CoSoSpot.Row)+CoSoSpot.StartIndex:(Colums*CoSoSpot.Row)+CoSoSpot.StartIndex+CoSoSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.MaPhong)
+			copy(ref[(Colums*PhongSpot.Row)+PhongSpot.StartIndex:(Colums*PhongSpot.Row)+PhongSpot.StartIndex+PhongSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.TenGiangVien)
+			copy(ref[(Colums*GiangVienSpot.Row)+GiangVienSpot.StartIndex:(Colums*GiangVienSpot.Row)+GiangVienSpot.StartIndex+GiangVienSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.MaNhom)
+			copy(ref[(Colums*NhomSpot.Row)+NhomSpot.StartIndex:(Colums*NhomSpot.Row)+NhomSpot.StartIndex+NhomSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.MaToTh)
+			copy(ref[(Colums*NhomTHSpot.Row)+NhomTHSpot.StartIndex:(Colums*NhomTHSpot.Row)+NhomTHSpot.StartIndex+NhomTHSpot.Length], []rune(tmp.String()))
+
+		} else {
+			ref = []rune(RefBottom)
+
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v - %v", thoiGianBatDau, thoiGianKetThuc)
+			copy(ref[(Colums*ThoiGianSpot.Row)+ThoiGianSpot.StartIndex:(Colums*ThoiGianSpot.Row)+ThoiGianSpot.StartIndex+ThoiGianSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.SoTiet)
+			copy(ref[(Colums*TietSpot.Row)+TietSpot.StartIndex:(Colums*TietSpot.Row)+TietSpot.StartIndex+TietSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.TenMon)
+			copy(ref[(Colums*MonSpot.Row)+MonSpot.StartIndex:(Colums*MonSpot.Row)+MonSpot.StartIndex+MonSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.MaCoSo)
+			copy(ref[(Colums*CoSoSpot.Row)+CoSoSpot.StartIndex:(Colums*CoSoSpot.Row)+CoSoSpot.StartIndex+CoSoSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.MaPhong)
+			copy(ref[(Colums*PhongSpot.Row)+PhongSpot.StartIndex:(Colums*PhongSpot.Row)+PhongSpot.StartIndex+PhongSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.TenGiangVien)
+			copy(ref[(Colums*GiangVienSpot.Row)+GiangVienSpot.StartIndex:(Colums*GiangVienSpot.Row)+GiangVienSpot.StartIndex+GiangVienSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.MaNhom)
+			copy(ref[(Colums*NhomSpot.Row)+NhomSpot.StartIndex:(Colums*NhomSpot.Row)+NhomSpot.StartIndex+NhomSpot.Length], []rune(tmp.String()))
+			tmp.Reset()
+			fmt.Fprintf(&tmp, "%v", tiet.MaToTh)
+			copy(ref[(Colums*NhomTHSpot.Row)+NhomTHSpot.StartIndex:(Colums*NhomTHSpot.Row)+NhomTHSpot.StartIndex+NhomTHSpot.Length], []rune(tmp.String()))
+		}
+
+		scheduleBytes = append(scheduleBytes[0:len(scheduleBytes)-Colums-1], []rune(ref)...)
+	}
+
+	return scheduleBytes, nil
 }
 
 func main() {
